0bda130ff07433f4ee27d73d15457bc7e570a80b
ettzzz <ertenzdya2005@gmail.com>
2021-09-02 12:37:46 +0800

    fix: update MTB_IP

--- config/static_vars.py
+++ config/static_vars.py
@@ -19,7 +19,7 @@ else:
 NEWS_HISTORY_PATH = os.path.join(ROOT, 'trade_news.db')
 DAY_ZERO = '2019-01-01'
 UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0'
-MTB_IP = 'https://api.ettzzz.nl'
+MTB_IP = 'https://www.ettzzz.ga'
 BOT_DISPATCH_ADDRESS = '{}/api_v1/send_message'.format(MTB_IP)
 API_PREFIX = 'api_v1'
 
